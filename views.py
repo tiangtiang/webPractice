5237a46cf9d42d0c8067e779dbf302963fb0cbc8
tiangloveannie@163.com <tiangloveannie@163.com>
2015-08-15 22:14:43 +0800

    模板继承

--- views.py
+++ views.py
@@ -1,8 +1,14 @@
 __author__ = '天祥'
 
 from init import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'tiang'}
+    posts = [
+        {'author': {'name': 'ling'}, 'body': 'tiang is a sb!'},
+        {'author': {'name': 'annie'}, 'body': 'go to type code!!!'}
+    ]
+    return render_template('index.html', user = user, title = 'Home Page', posts = posts)
